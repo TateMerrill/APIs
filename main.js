0910aa9c6db315b613d17a6040ce44e4610948c7
Tate Merrill <tatemerrill@gmail.com>
2021-08-18 15:33:03 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('button') 
+
+button.addEventListener('click', (e) =>{ 
+
+    axios.get('https://swapi.dev/api/planets/2').then((res) => {
+        for(let i = 0; i <res.data.residents.length; i++){
+            console.log(res.data.residents[i])
+            axios.get(res.data.residents[i]).then((res2) => {
+                let resident = document.createElement('h2')
+                resident.textContent = `${res2.data.name}`
+                document.querySelector('body').appendChild(resident)
+            })
+        }
+    }).catch((err) => {
+        console.log(`Something not right I can feel it.`)
+    });
+})
